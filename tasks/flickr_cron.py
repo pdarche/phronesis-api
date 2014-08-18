6a329304e1cf7e18d07aa05b67bc09e083e37728
Peter Darche <pdarche@gmail.com>
2014-08-17 22:33:35 -0400

    Add to flickr cron

--- tasks/flickr_cron.py
+++ tasks/flickr_cron.py
@@ -21,11 +21,11 @@ photo_id_dicts = list(db.photos.find({},{"_id":0, 'id':1}))
 existing_photo_ids = [p['id'] for p in photo_id_dicts]
 
 # for each photo in the set 
-for photo in foods_set[:10]:
+for photo in foods_set:
 	photo_id = photo.get('id')
 	# if the photo_id isn't already in flickr photos
-    # get the photo
-    if photo_id not in existing_photo_ids:
+	# get the photo
+	if photo_id not in existing_photo_ids:
 		photo_info_json = flickr.photos_getInfo(photo_id=photo_id, format='json')
 		photo_info = json.loads(photo_info_json[14:-1])
 		db.photos.insert(photo_info['photo'])
